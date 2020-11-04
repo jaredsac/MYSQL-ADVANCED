ce05cb6d6d9e23ba10fb53744d39eea890553d38
Jared Sackey <jared.sackey@student.rocva.nl>
2020-11-04 15:01:32 +0100

    klaar

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,3 @@
 
+INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+    VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8])
--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,3 @@
 
+INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `woonplaats`) 
+VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6])
