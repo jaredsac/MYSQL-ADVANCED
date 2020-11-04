252daabe6bcdd3a9e7ceb57387a10675c71d294e
Jared Sackey <jared.sackey@student.rocva.nl>
2020-11-04 15:01:50 +0100

    bijna

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,7 +1,7 @@
 -- Opdracht 1
-
+UPDATE `studenten2` SET `woonplaats`= "Amstelveen" 
 -- Opdracht 2 
-
+UPDATE `studenten2` SET `adres`= "HabboeStraat",`postcode`= "1578PW" WHERE `student_id`= 8
 -- Opdracht 3
 
 -- Opdracht 4
