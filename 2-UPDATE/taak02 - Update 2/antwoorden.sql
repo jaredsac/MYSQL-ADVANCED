bcaa719c723065287305febf6ebc1b354beb1b44
Jared Sackey <jared.sackey@student.rocva.nl>
2020-11-04 17:19:03 +0100

    klaaar

--- 2-UPDATE/taak02 - Update 2/antwoorden.sql
+++ 2-UPDATE/taak02 - Update 2/antwoorden.sql
@@ -1,8 +1,8 @@
 -- Opdracht 1
-
+UPDATE `studenten2` SET `klas` = '9X' WHERE `studenten2`.`klas` = '9A' 
 -- Opdracht 2 
-
+UPDATE `studenten2` SET `klas` = '9Z' WHERE `studenten2`.`student_id` > 7 
 -- Opdracht 3
-
+UPDATE `studenten2` SET `woonplaats` = 'Amsterdam' WHERE `student_id` <= 2 or `student_id` > 7 
 -- Opdracht 4
-
+UPDATE `studenten2` SET `woonplaats` = 'Uithoorn' WHERE `klas` = '9X' AND `woonplaats` = 'Amsterdam' 
